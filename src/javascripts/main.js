aa00915ee2992e838fdb7b24b9910a87ceedc218
Sean <jb@JBs-MacBook-Pro.local>
2021-04-10 18:12:27 -0500

    added netlify deploy

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,11 +3,10 @@
 import 'bootstrap'; // import bootstrap elements and js
 
 import '../styles/main.scss';
+import checkLoginStatus from './helpers/auth';
 
 const init = () => {
-  document.querySelector('#app').innerHTML = '<h1>HELLO! You are up and running!</h1>';
-  console.warn('YOU ARE UP AND RUNNING!');
-
+  checkLoginStatus();
   // USE WITH FIREBASE AUTH
   // checkLoginStatus();
 };
