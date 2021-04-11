fb9de1f34d1a63156e5e3a48272726957dcbd0bc
Sean <jb@JBs-MacBook-Pro.local>
2021-04-11 18:37:15 -0500

    ignore

--- src/javascripts/helpers/data/weatherCall.js
+++ src/javascripts/helpers/data/weatherCall.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import firebaseConfig from '../apiKeys';
 
+const dbUrl = firebaseConfig.databaseURL;
+
 // MAKES CALL TO WEATHER DATABASE
 const weatherCall = (location) => new Promise((resolve, reject) => {
   axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${firebaseConfig.weatherApiKey}`)
@@ -16,12 +18,17 @@ const weatherCall = (location) => new Promise((resolve, reject) => {
 
 // CREATES NEW LOCATION OF WEATHER IN FIREBASE
 const newFirebaseWeatherObj = (location) => new Promise((reject) => {
-  axios.post('https://sr-api-website-default-rtdb.firebaseio.com/weather.json', location)
+  axios.post(`${dbUrl}/weather.json`, location)
     .then((response) => {
       const body = { firebaseKey: response.data.name };
-      axios.patch(`https://sr-api-website-default-rtdb.firebaseio.com/weather/${response.data.name}.json`, body);
+      axios.patch(`${dbUrl}/weather/${response.data.name}.json`, body);
     })
     .catch((error) => reject(error));
 });
+
+// READS ALL LOCATIONS IN FIREBASE BASED OFF OF UID
+const getWeather = (uid) => new Promise((resolve, reject) => {
+  axios.get(`${}`)
+})
 
 export { weatherCall, newFirebaseWeatherObj };
