48507c6e5833187b628895519ae99d268e0dfc5b
Sean <jb@JBs-MacBook-Pro.local>
2021-04-12 21:24:28 -0500

    added a few modules

--- src/javascripts/components/domBuilder.js
+++ src/javascripts/components/domBuilder.js
@@ -1,19 +1,7 @@
 const domBuilder = () => {
   document.querySelector('#app').innerHTML = `
-    <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-4">Get Weather Info</h1>
-        <p class="lead">Enter a Location Below to Get Current Weather</p>
-        <form>
-          <div class="mb-3 form-group">
-            <label for="weather-location-input" class="form-label">Location</label>
-            <input type="text" class="form-control" aria-describedby="location" id="weather-location-input" placeholder="Enter a City" required>
-          </div>
-        </form>
-        <button type="button" class="btn btn-primary" id="weather-location-submit">Submit</button>
-      </div>
-    </div>
-    <div class="container d-flex flex-wrap" id="weather-cards"></div>
+    <div id="form"></div>
+    <div id="cards"></div>
   `;
 };
 
--- src/javascripts/views/startApp.js
+++ src/javascripts/views/startApp.js
@@ -1,10 +1,12 @@
 import domBuilder from '../components/domBuilder';
+import weatherForm from '../components/forms/weatherForm';
 import logoutButton from '../components/logoutButton';
 import domEvents from '../events/domEvents';
 
 const startApp = (user) => {
   logoutButton();
   domBuilder();
+  weatherForm();
   domEvents(user.uid);
 };
 
