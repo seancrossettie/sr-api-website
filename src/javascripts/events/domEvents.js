d72c2a9584f4c93a15bc75579cbb6537b938f729
Sean <jb@JBs-MacBook-Pro.local>
2021-04-16 19:42:22 -0500

    added navbar

--- src/javascripts/events/domEvents.js
+++ src/javascripts/events/domEvents.js
@@ -1,23 +1,27 @@
-import firebase from 'firebase/app';
+// import firebase from 'firebase/app';
 import 'firebase/auth';
 import showWeather from '../components/showWeather';
-import { newFirebaseWeatherObj, weatherCall } from '../helpers/data/weatherCall';
+import { weatherCall } from '../helpers/data/weatherCall';
 
 const domEvents = () => {
-  document.querySelector('body').addEventListener('click', (e) => {
-    if (e.target.id.includes('weather-location-submit')) {
-      const weatherLocation = document.querySelector('#weather-location-input').value;
-      const firebaseWeatherObj = {
-        location: document.querySelector('#weather-location-input').value,
-        uid: firebase.auth().currentUser.uid,
-      };
-      if (weatherLocation) {
-        weatherCall(weatherLocation)
-          .then((weather) => showWeather(weather));
-        newFirebaseWeatherObj(firebaseWeatherObj);
-        document.querySelector('#weather-location-input').value = '';
-      }
+  document.querySelector('#weather-location-submit').addEventListener('click', (e) => {
+    e.preventDefault();
+    const weatherLocation = document.querySelector('#weather-location-input').value;
+    // const firebaseWeatherObj = {
+    //   location: document.querySelector('#weather-location-input').value,
+    //   uid: firebase.auth().currentUser.uid,
+    // };
+    if (weatherLocation) {
+      weatherCall(weatherLocation)
+        .then((weather) => showWeather(weather));
+      // newFirebaseWeatherObj(firebaseWeatherObj);
+      document.querySelector('#weather-location-input').value = '';
     }
+
+    // if (e.target.id.includes('#untrack-weather')) {
+    //   const firebaseKey = e.target.id.split('--')[1];
+    //   console.warn(firebaseKey);
+    // }
   });
 };
 
